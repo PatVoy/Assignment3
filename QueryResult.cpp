2d2bc7b1d1378deb530a0203642ac79c2d5f8aa2
Petio Petrov <petioptrv@icloud.com>
2017-10-14 13:30:42 -0400

    Update QueryResult.cpp

--- QueryResult.cpp
+++ QueryResult.cpp
@@ -1,15 +1,25 @@
-//
-//  QueryResult.cpp
-//  
-//
-//  Created by Petio Petrov on 2017-10-13.
-//
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   QuerryResult.cpp
+ * Author: petioptrv
+ * 
+ * Created on October 14, 2017, 1:06 PM
+ */
 
 #include "QueryResult.h"
 
-QueryResult::QueryResult(document d, int s) : doc(d), score(s) {}
-
-std::ostream & operator<< (std::ostream & os, const QueryResult & queryResult) {
-    std::ostringstream buffer;
-    buffer << doc.name() << "\n" << score;
+QueryResult::QueryResult(const Document & d, const int & s) : doc(d), score(s) {
 }
+
+std::ostream & operator<<(std::ostream & os, const QueryResult & queryResult) {
+    os << "DOC SCORE: " << queryResult.score << "\n\n";
+    os << "DOC PRINTOUT\n" <<  queryResult.doc;
+    
+    return os;
+}
+
