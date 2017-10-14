8823b8878a0385de7b814c0d34e80e38d7f14353
Petio Petrov <petioptrv@icloud.com>
2017-10-14 12:56:43 -0400

    Update Document.h

--- Document.h
+++ Document.h
@@ -1,25 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Document.h
+ * Author: petioptrv
+ *
+ * Created on October 14, 2017, 11:35 AM
+ */
+
 #ifndef DOCUMENT_H
 #define DOCUMENT_H
+
 #include <string>
 #include <vector>
-#include <map>
+#include <fstream>
 
 class Document
 {
-    public:
-        document();
-        document(std::string fileName);
-        std::string name();
-        int size();
-        void content();
-
-    private:
-        std::string fileName;
-        std::vector<std::string> contentV;
-        std::map< std::string, std::map<std::string,int> > wordMap;
-        std::map<std::string,int> fullWordTotals;
-        size_t maxWordLen;
-
+public:
+    Document(const std::string & name);
+    const std::string name();
+    const int size();
+    const std::string content();
+    friend std::ostream & operator<<(std::ostream & os, Document & doc);    
+private:
+    std::string fileName;
+    std::string docContent;
 };
 
-#endif // DOCUMENT_H
+#endif /* DOCUMENT_H */
+
