5971f1a95fad42b71430beba8a6baa2065437fe7
PatVoy <32780684+PatVoy@users.noreply.github.com>
2017-10-14 15:18:05 -0400

    Isabella stopwords file
    kept Petios overloaded () operator

--- Stopwords.cpp
+++ Stopwords.cpp
@@ -1,41 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+#include "stopword.h"
+#include <iostream>
+#include <fstream>
+#include <list>
 
-/* 
- * File:   Stopwords.cpp
- * Author: petioptrv
- * 
- * Created on October 14, 2017, 11:25 AM
- */
+using namespace std;
 
-#include "Stopwords.h"
-
-// Default constructor
-Stopwords::Stopwords() {
-    std::string fileName = "stopwords.txt";
-    std::ifstream fin(fileName.c_str());
-    
-    std::string word;
-    // insert words into list
-    while (fin >> word)
-        vectorStopwords.push_back(word);
-    fin.close();
-}
-
-// Constructor reads the stopword list from this file
-Stopwords::Stopwords(const std::string & fileName) {
-    std::ifstream fin(fileName.c_str());
-    
-    std::string word;
-    // insert words into list
-    while (fin >> word)
-        vectorStopwords.push_back(word);
-    fin.close();
-}
-
+    //default constructor
+    stopword::stopword()
+    {
+    }
+    // constructor reads the stopword list from this file
+    stopword::stopword(string fileName)
+    {
+       ifstream fin(fileName.c_str());
+       string word;
+       // insert words into list
+       while (fin >> word)
+        {
+        listStopwords.push_back(word);
+        }
+        fin.close();
+    }
+    // overload operator<< returns stopword list content
+    std::ostream &operator<< (std::ostream & os,const stopword & st)
+    {
+       os << "Stopwords: " << endl;
+       for (list<string>:: const_iterator i = st.listStopwords.begin(); i != st.listStopwords.end(); ++i)
+            os << *i << endl;
+        return os;
+    }
 const bool Stopwords::operator() (const std::string & word) {
     for (std::vector<std::string>:: const_iterator i = vectorStopwords.begin();
          i != vectorStopwords.end();
@@ -43,15 +36,3 @@ const bool Stopwords::operator() (const std::string & word) {
         if (word == *i) return true;
     return false;
 }
-
-// overload operator<< returns stopword list content
-std::ostream &operator<< (std::ostream & os,const Stopwords & st) {
-    // Cannot use endl here because endl flushes the buffer
-    os << "Stopwords: " << "\n";
-    for (std::vector<std::string>::const_iterator i = st.vectorStopwords.begin();
-         i != st.vectorStopwords.end();
-         ++i)
-        os << *i << "\n";
-    return os;
-}
-
