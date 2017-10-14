aa74323ca6b4dd7f6fe4675254b9d9c4a3e99a0f
Petio Petrov <petioptrv@icloud.com>
2017-10-14 14:01:48 -0400

    Update Indexer.cpp

--- Indexer.cpp
+++ Indexer.cpp
@@ -1,47 +1,97 @@
-#include "Indexer.h"  // This should be the only import in here
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
- // constructor
-Indexer::Indexer()
-{
-  Indexer::numDoc = 0;
-  Indexer::normalized = false;
+/* 
+ * File:   Indexer.cpp
+ * Author: petioptrv
+ * 
+ * Created on October 14, 2017, 1:31 PM
+ */
+
+#include "Indexer.h"
+
+Indexer::Indexer() {
+    normalized = false;
 }
 
-int Indexer::size()
-{
-   return numDoc;
+const size_t Indexer::size() {
+    return docNames.size();
 }
 
-void Indexer::printMatrix() {}
+void Indexer::printMatrix() {
 
-void Indexer::printMatrixReduced() {}
-
-void Indexer::normalize()
-{
-    normalized = ans;  // Flag matrix as normalized
 }
 
-void Indexer::setNormalized(bool ans)
-{
+void Indexer::printReducedMatrix() {
+
 }
-//adding a document into the index
-void add(document::)
-{
-    indx.setNormalized(false);
-    indx.doc_names.push_back(doc.name());
-    std::map < std::string, std::map < document, std::pair < int, double > > > :: iterator wordCheck;
-    std::string words[] = {"a", "b", "c", "a", "b", "d"};
-    for(int i = 0; i < 6; i++){
-        if(indx.wordMap.empty())
-            indx.wordMap[words[i]].insert({doc, {1, 0}});
-        else
-        {
-            if(indx.wordMap.count(words[i]) == 0)
-                indx.wordMap[words[i]].insert({doc, {1, 0}});
-            else
-                indx.wordMap[words[i]][doc].first += 1;
+
+void Indexer::normalize() {
+
+}
+
+void Indexer::add(const Document & doc) {
+    normalized = false;
+    std::string docName = doc.name();  // The reason I save the value to a local variable is because this prevents us from having to make a method call (i.e. doc.name()) everytime we need the name of the document, which is needed on each iteration while adding words to the database below – this is just faster
+    docNames.push_back(docName);
+    
+    // Extract words from doc content
+    Tokenizer tokenizer();  // Create tokenizer object
+    // Feed the content of the doc to the tokenizer overloaded braket operator
+    // to get a vector of words in return
+    std::vector<std::string> tokens = tokenizer(doc.content());
+    
+    // Travers the vector of tokens and add it to the data structure
+    for (std::vector<std::string>::const_iterator token = tokens.begin();
+         token != tokens.end();
+         ++token) {
+        wordMap[*token][docName]["frequency"]++;
+    }
+}
+
+const std::vector<QueryResult> query(std::string & query) {
+    
+}
+
+const std::vector<QueryResult> query(std::string & query, int & querySize) {
+    
+}
+
+std::ostream & operator<<(std::ostream * os, const Indexer i) {
+    // Print the normalized boolean
+    os << "Normalized: " << i.normalized << "\n";
+    
+    // Iterate through every word
+    for (std::map<std::string, std::map<std::string, 
+            std::map<std::string, float> > >::const_iterator 
+            word = i.wordMap.begin();                           // THIS LOOKS HORRIBLE!!!!
+         word != i.wordMap.end();
+         ++word) {
+        
+        os << word->first << "\n";  // Print out the word
+        // Iterate over every document
+        for (std::vector<std::string>::const_iterator 
+                docName = i.docNames.begin();
+             docName != i.docNames.end();
+             ++docName) {
+            
+            os << "    " << *docName << "\n";  // Print out the doc name
+            // Iterate over the different metrics for that word within that document
+            for (std::map<word, float>::const_iterator 
+                    metric = word->second[docName].begin();
+                 metric != word->second[docName].end();
+                 ++metric) {
+                
+                // Print out the metric string and the value associated with it
+                os << "        ";  // Indent output
+                os << metric->first << ": " << metric->second << "\n";
+            }
         }
     }
     
-    numDoc++;  // Increment doc counter
+    return os;
 }
+
