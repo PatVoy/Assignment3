b38b82af693d56f8edacb1a7c9214a79d9e1e643
Petio Petrov <petioptrv@icloud.com>
2017-10-14 13:11:13 -0400

    Update Tokenizer.h

--- Tokenizer.h
+++ Tokenizer.h
@@ -1,20 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Tokenizer.h
+ * Author: petioptrv
+ *
+ * Created on October 14, 2017, 11:58 AM
+ */
+
 #ifndef TOKENIZER_H
 #define TOKENIZER_H
+
+#include <string>
 #include <fstream>
-#include <istream>
 #include <vector>
 
-
-class tokenizer
-{
-    public:
-        tokenizer();
-        tokenizer(std::ifstream fin);
-
-
-    private:
-        std::vector<std::string> tokenV;
-
+class Tokenizer {
+public:
+    Tokenizer();
+    Tokenizer(const std::string & fileName);
+    const std::string getDelimiters();
+    std::vector<std::string> operator()(std::string & inputStr);
+    friend std::ostream & operator<<(std::ostream & os, Tokenizer & tokenizer);
+private:
+    std::string delimiters;
 };
 
-#endif // TOKENIZER_H
+#endif /* TOKENIZER_H */
+
