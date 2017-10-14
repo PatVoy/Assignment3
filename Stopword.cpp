3b82714d050941a9e1775e1d5a4a901b65c44b76
Petio Petrov <petioptrv@icloud.com>
2017-10-14 12:57:20 -0400

    Update Stopword.cpp

--- Stopword.cpp
+++ Stopword.cpp
@@ -1,24 +1,57 @@
-#include "stopword.h"
-#include <iostream>
-#include <fstream>
-#include <list>
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
-using namespace std;
-    //std::list<std::string> lStopwords;
+/* 
+ * File:   Stopwords.cpp
+ * Author: petioptrv
+ * 
+ * Created on October 14, 2017, 11:25 AM
+ */
 
-    //default constructor creates anobject with an empty stopword list
-    stopword::stopword()
-    {
-    }
-    //reads the stopword list from this file
-    stopword::stopword(string fileName)
-    {
-        ifstream fin(fileName.c_str());
-       string word;
-       while (fin >> word)
-        {
-        listStopwords.push_back(word);
-        }
-        fin.close();
+#include "Stopwords.h"
 
-    }
+// Default constructor
+Stopwords::Stopwords() {
+    std::string fileName = "stopwords.txt";
+    std::ifstream fin(fileName.c_str());
+    
+    std::string word;
+    // insert words into list
+    while (fin >> word)
+        vectorStopwords.push_back(word);
+    fin.close();
+}
+
+// Constructor reads the stopword list from this file
+Stopwords::Stopwords(std::string fileName) {
+    std::ifstream fin(fileName.c_str());
+    
+    std::string word;
+    // insert words into list
+    while (fin >> word)
+        vectorStopwords.push_back(word);
+    fin.close();
+}
+
+const bool Stopwords::operator() (const std::string & word) {
+    for (std::vector<std::string>:: const_iterator i = vectorStopwords.begin();
+         i != vectorStopwords.end();
+         ++i)
+        if (word == *i) return true;
+    return false;
+}
+
+// overload operator<< returns stopword list content
+std::ostream &operator<< (std::ostream & os,const Stopwords & st) {
+    // Cannot use endl here because endl flushes the buffer
+    os << "Stopwords: " << "\n";
+    for (std::vector<std::string>::const_iterator i = st.vectorStopwords.begin();
+         i != st.vectorStopwords.end();
+         ++i)
+        os << *i << "\n";
+    return os;
+}
+
